969eb262132c3d4c749f385b34d2ef9684ea83dc
Hung Tang <hungtang@hotmail.com>
2021-06-23 23:54:23 -0700

    created apollo server-side schemas for typeDefs and resolvers and updated server.js to instantiate apollo server to work with express

---
+++ server/schemas/resolvers.js
@@ -0,0 +1,52 @@
+const { Book, User } = require('../models');
+const { AuthenticationError } = require('apollo-server-express');
+const { signToken } = require('../utils/auth');
+const auth = require('../utils/auth');
+
+const resolvers = {
+    Query: {
+        me: async (_root, args, context) => {
+            if (context.user) {
+                const userData = await User.findOne({})
+                    .select('-__v -password')
+                    .populate('books');
+                
+                return userData;
+            }
+
+            throw new AuthenticationError('Not logged in!');
+        },
+        users: async () => {
+            return User.find()
+                .select('-__v -password')
+                .populate('books');
+        }
+    },
+    Mutation: {
+        login: async (_root, { email, password }) => {
+            const user = await User.findOne({ email });
+
+            if (!user) {
+                throw new AuthenticationError('Incorrect credentials)');
+            }
+
+            const correctPw = await user.isCorrectPassword(password);
+
+            if (!correctPw) {
+                throw new AuthenticationError('Incorrect credentials)');
+            }
+
+            const token = signToken(user);
+            return { token, user };
+        },
+        addUser: async (_root, args) => {
+            const user = await User.create(args);
+            const token = signToken(user);
+
+            return { token, user };
+        },
+    }
+};
+
+// exports resolvers
+module.exports = resolvers;
--- server/server.js
+++ server/server.js
@@ -1,10 +1,23 @@
 const express = require('express');
 const path = require('path');
+// import ApolloServer
+const { ApolloServer } = require('apollo-server-express');
+// import typeDefs and resolvers
+const { typeDefs, resolvers } = require('./schemas');
 const db = require('./config/connection');
-const routes = require('./routes');
+// const routes = require('./routes');  // commenting out - replaced by graphql
 
 const app = express();
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT || 6001;
+
+// Create a new Apollo server and pass in gql schema data
+const server = new ApolloServer({
+  typeDefs,
+  resolvers
+});
+
+// integrate Apollo server with express app as middleware
+server.applyMiddleware({ app });
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
@@ -14,8 +27,12 @@ if (process.env.NODE_ENV === 'production') {
   app.use(express.static(path.join(__dirname, '../client/build')));
 }
 
-app.use(routes);
+// app.use(routes); // commenting out as this uses api routes
 
 db.once('open', () => {
   app.listen(PORT, () => console.log(`🌍 Now listening on localhost:${PORT}`));
+  
+  // log where we can go to test GQL API
+  console.log(`Use GraphQL at http://localhost:${PORT}${server.graphqlPath}`);
+
 });

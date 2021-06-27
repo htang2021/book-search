f8d8a883fe7acb497cb974a1841a07057373fb8a
Hung Tang <hungtang@hotmail.com>
2021-06-27 15:08:05 -0700

    updated login-form with graphql mutations

--- client/src/components/LoginForm.js
+++ client/src/components/LoginForm.js
@@ -1,12 +1,16 @@
-// see SignupForm.js for comments
 import React, { useState } from 'react';
 import { Form, Button, Alert } from 'react-bootstrap';
+// intead of api routes, use graphql mutation and jwt to login users
+import { useMutation } from '@apollo/client';
+import { LOGIN_USER } from '../utils/mutations';
 
-import { loginUser } from '../utils/API';
 import Auth from '../utils/auth';
 
 const LoginForm = () => {
   const [userFormData, setUserFormData] = useState({ email: '', password: '' });
+  
+  const [login, { error }] = useMutation(LOGIN_USER);
+  
   const [validated] = useState(false);
   const [showAlert, setShowAlert] = useState(false);
 
@@ -18,8 +22,6 @@ const LoginForm = () => {
   const handleFormSubmit = async (event) => {
     event.preventDefault();
 
-    
-    console.log(userFormData);
     // check if form has everything (as per react-bootstrap docs)
     const form = event.currentTarget;
     if (form.checkValidity() === false) {
@@ -28,15 +30,11 @@ const LoginForm = () => {
     }
 
     try {
-      const response = await loginUser(userFormData);
+      const { data } = await login({
+        variables: {...userFormData }
+      });
 
-      if (!response.ok) {
-        throw new Error('something went wrong!');
-      }
-
-      const { token, user } = await response.json();
-      console.log(user);
-      Auth.login(token);
+      Auth.login(login.token);
     } catch (err) {
       console.error(err);
       setShowAlert(true);
